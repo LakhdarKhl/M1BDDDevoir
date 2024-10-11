649bd2b3ac6eda4452ccd2177fd031fed561b902
Samuel-lgd <lagarde.samuel@gmail.com>
2024-10-11 09:54:38 +0200

    project init

---
+++ src/main/java/com/m1/bdd_merise_m1/BddMeriseM1Application.java
@@ -0,0 +1,13 @@
+package com.m1.bdd_merise_m1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddMeriseM1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BddMeriseM1Application.class, args);
+    }
+
+}
