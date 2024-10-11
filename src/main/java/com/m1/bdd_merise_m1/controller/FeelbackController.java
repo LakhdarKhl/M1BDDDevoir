c811c58fdfcba4207451ea4f7bed994e2aaedde5
Samuel-lgd <lagarde.samuel@gmail.com>
2024-10-11 11:50:00 +0200

    dashboard page

--- src/main/java/com/m1/bdd_merise_m1/controller/FeelbackController.java
+++ src/main/java/com/m1/bdd_merise_m1/controller/FeelbackController.java
@@ -1,7 +1,10 @@
 package com.m1.bdd_merise_m1.controller;
 
+import com.m1.bdd_merise_m1.dto.StatsDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class FeelbackController {
@@ -12,7 +15,16 @@ public class FeelbackController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+
+        StatsDTO stats = new StatsDTO();
+        stats.setAnswersCount(10);
+        stats.setDelayRate(0.5);
+        stats.setPackageStateRate(0.8);
+        stats.setDeliveryRate(0.9);
+        
+        model.addAttribute("stats", stats);
+        
         return "dashboard";
     }
 
